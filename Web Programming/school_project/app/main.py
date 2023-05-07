38ba29441a5d83e28f22c1469a20fab3ca708530
Eugene Jun <genenamu@gmail.com>
2023-05-07 17:09:59 +0900

    feat: add links and change some code

--- Web Programming/school_project/app/main.py
+++ Web Programming/school_project/app/main.py
@@ -8,7 +8,29 @@ app.mount("/static", StaticFiles(directory=f"{current_dir}/pages/static"), name=
 templates = Jinja2Templates(directory= f"{current_dir}/pages")
 @app.get("/")
 def read_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-@app.get("/bye")
+    return templates.TemplateResponse("main_page.html", {"request": request})
+    
+@app.get("/1st")
 def read_bye_page(request: Request):
-    return templates.TemplateResponse("bye.html", {"request": request})
+    return templates.TemplateResponse("1st.html", {"request": request})
+@app.get("/2nd")
+def read_bye_page(request: Request):
+    return templates.TemplateResponse("2nd.html", {"request": request})
+@app.get("/3rd")
+def read_bye_page(request: Request):
+    return templates.TemplateResponse("3rd.html", {"request": request})
+@app.get("/4th")
+def read_bye_page(request: Request):
+    return templates.TemplateResponse("4th.html", {"request": request})
+
+@app.get("/study_time")
+def read_index(request: Request):
+    return templates.TemplateResponse("study_time.html", {"request": request})
+
+@app.get("/ranking")
+def read_index(request: Request):
+    return templates.TemplateResponse("time_rank.html", {"request": request})
+
+@app.get("/personal")
+def read_index(request: Request):
+    return templates.TemplateResponse("personal.html", {"request": request})
